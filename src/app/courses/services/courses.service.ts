746d6a893efaf1452ee350fea55d4d2086da37ae
ramilani12 <ramilani@gmail.com>
2023-01-30 16:37:27 -0300

    Added courses json

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import {HttpClient} from '@angular/common/http';
+import { first, take, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient : HttpClient) { }
 
 
-  list() : Course[] {
-    return [
-      {
-        _id : "1" , name : "Angular" , category : "Frontend"
-      }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 
 }
